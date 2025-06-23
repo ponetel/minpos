caa2344587c02b5c0d59efcbeb1857f657868d33
ponetel <ponetel@gmail.com>
2025-06-23 20:37:50 +0700

    upload to github

---
+++ resources/views/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=h1, initial-scale=1.0">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <h1>Laravel 12</h1>
+    <hr>
+    <div id="app"></div>
+
+@vite('resources/js/app.js')
+</body>
+</html>
